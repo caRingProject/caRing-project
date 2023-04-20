94b76bd51e93139294e668328c2321f334c0b010
Yoon <heo4604@gmail.com>
2023-04-20 00:06:20 -0700

    first commit

---
+++ SpringBoot_caRing/src/main/java/com/example/caRing/SpringBootCaRingApplication.java
@@ -0,0 +1,13 @@
+package com.example.caRing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCaRingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCaRingApplication.class, args);
+	}
+
+}
---
+++ SpringBoot_caRing/src/test/java/com/example/caRing/SpringBootCaRingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.caRing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCaRingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
